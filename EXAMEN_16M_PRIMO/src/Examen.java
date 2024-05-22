603374236f96f30e5becc97f3e6aa87f84295345
David <David@LAPTOP-PSG6E8DL>
2024-05-22 12:08:18 +0200

    Examen

---
+++ EXAMEN_16M_PRIMO/src/Examen.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Examen {
+	
+	public static void main (String [] arg) {
+		Scanner ent = new Scanner(System.in);
+		int numero = 0; 
+		
+		System.out.println("Buenos dias Ceinmark, soy David");
+		
+		System.out.println("Introduce un numero");
+		numero = Integer.parseInt(ent.nextLine());
+	}
+
+}
